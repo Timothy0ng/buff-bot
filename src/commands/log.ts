54e9fa75b2b00a7484ec3d1d6214f57f4895fdfc
Timothy0ng <2101237b@student.tp.edu.sg>
2023-04-17 22:54:17 +0800

    feat(log): log within same channel

--- src/commands/log.ts
+++ src/commands/log.ts
@@ -21,9 +21,25 @@ export class UserCommand extends Command {
   }
 
   public override async chatInputRun(interaction: Command.ChatInputCommandInteraction) {
-    const username = interaction.user.username;
+    const messageId: string | null = interaction.options.getString("msg_id");
+
+    if (!messageId) {
+      return interaction.reply({ content: "Please provide a valid message ID", ephemeral: true });
+    }
+
+    const message = await interaction.channel?.messages.fetch(messageId);
+
+    if (!message) {
+      return interaction.reply({
+        content: "Could not find the specified message",
+        ephemeral: true
+      });
+    }
+
+    const content = message.content;
+    const userId = message.author.username;
     return interaction.reply({
-      content: `Well done! @${username} has just completed the following: \n`
+      content: `Well done! @${userId} has completed the following: \n ${content}`
     });
   }
 }
