d4854ff2e3d85a8de154029d8730970f1faff9d5
TMK04 <floer.learner.01@gmail.com>
2023-04-24 22:45:41 +0800

    feat(db): setup reminder table

--- src/@types/knex.d.ts
+++ src/@types/knex.d.ts
@@ -8,7 +8,17 @@ declare module "knex/types/tables" {
     message_id: string;
   }
 
+  interface Reminder {
+    id: number;
+    user_id: string;
+    guild_id: string;
+    dow: number;
+    h: number;
+    m: number;
+  }
+
   interface Tables {
     log: Log;
+    reminder: Reminder;
   }
 }
--- src/db/setup.ts
+++ src/db/setup.ts
@@ -1,6 +1,6 @@
 import { db } from ".";
 
-(async function () {
+async function setupLog() {
   const has_log = await db.schema.hasTable("log");
 
   if (!has_log) {
@@ -11,4 +11,21 @@ import { db } from ".";
       t.string("message_id").notNullable();
     });
   }
-})();
+}
+
+async function setupReminder() {
+  const has_reminder = await db.schema.hasTable("reminder");
+
+  if (!has_reminder) {
+    await db.schema.createTable("reminder", function (t) {
+      t.increments("id").unsigned();
+      t.string("user_id").notNullable();
+      t.string("guild_id").notNullable();
+      t.tinyint("dow").unsigned().notNullable();
+      t.tinyint("h").unsigned().notNullable();
+      t.tinyint("m").unsigned().notNullable();
+    });
+  }
+}
+
+Promise.all([setupLog(), setupReminder()]);
